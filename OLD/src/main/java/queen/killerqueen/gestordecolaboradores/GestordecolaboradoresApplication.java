9dd4acc65615cc6ecd49420196f1d8da3df78c23
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-27 20:21:35 -0300

    halfeat(postgres): integração db

---
+++ OLD/src/main/java/queen/killerqueen/gestordecolaboradores/GestordecolaboradoresApplication.java
@@ -0,0 +1,13 @@
+package queen.killerqueen.gestordecolaboradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestordecolaboradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestordecolaboradoresApplication.class, args);
+	}
+
+}
