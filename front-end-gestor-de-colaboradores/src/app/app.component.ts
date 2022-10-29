b909e3d213c0a4857b61396426b33b8b069b137a
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-28 22:53:30 -0300

    feat(crud): Crud finalizado

--- front-end-gestor-de-colaboradores/src/app/app.component.ts
+++ front-end-gestor-de-colaboradores/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Colaborador } from './colaborador';
 import { ColaboradorService } from './colaborador.service';
 
@@ -7,41 +9,96 @@ import { ColaboradorService } from './colaborador.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit{
-  public colaboradores: Colaborador[] | undefined;
+export class AppComponent implements OnInit {
+  public colaboradores!: Colaborador[];
+  public editColaborador!: Colaborador;
+  public deleteColaborador!: Colaborador;
 
+  public genericColaborador: Colaborador = {
+    id: 3,
+    nome: "Fulano",
+    email: "email@email.com",
+    cargo: "Cargo",
+    telefone: "9955640843",
+    imgUrl: '',
+    codigoColaborador: "1234",
+  }
   ngOnInit() {
+    this.colaboradorService.addColaborador(this.genericColaborador)
     this.getColaboradores();
   }
-  constructor(private colaboradorService: ColaboradorService) {}
+  constructor(private colaboradorService: ColaboradorService) { }
 
-  public getColaboradores(): void{
+  public getColaboradores(): void {
     this.colaboradorService.getColaboradores().subscribe(
-      (response: Colaborador[]) =>{
+      (response: Colaborador[]) => {
         this.colaboradores = response;
       },
-      ( error: any ) => {
-        alert ( error.message )
+      (error: any) => {
+        alert(error.message)
       }
     );
   }
 
-  public onAddColaborador(addForm: NgForm): void{
-    
+  public onAddColaborador(addForm: NgForm): void {
+    document.getElementById('add-colaborador-form')?.click();
+
+    this.colaboradorService.addColaborador(addForm.value).subscribe(
+      (response: Colaborador) => {
+        console.log(response);
+        this.getColaboradores();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+        addForm.reset();
+
+      }
+    )
   }
-  public onOpenModal(colaborador: Colaborador, mode: string): void{
+
+  public onUpdateColaborador(colaborador: Colaborador): void {
+    this.colaboradorService.updateColaborador(colaborador).subscribe(
+      (response: Colaborador) => {
+        console.log(response);
+        this.getColaboradores();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  public onDeleteColaborador(colaboradorId: number): void {
+    this.colaboradorService.deleteColaborador(colaboradorId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getColaboradores();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+
+
+
+  public onOpenModal(colaborador: Colaborador | null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
-    button.setAttribute('data-toggle','modal');
-    if( mode == 'add' ){
+    button.setAttribute('data-toggle', 'modal');
+    if (mode == 'add') {
       button.setAttribute('data-target', '#addColaboradorModal');
     }
-    if( mode == 'edit' ){
+    if (mode == 'edit') {
+      this.editColaborador = colaborador as Colaborador;
       button.setAttribute('data-target', '#updateColaboradorModal');
     }
-    if( mode == 'delete' ){
+    if (mode == 'delete') {
+      this.deleteColaborador = colaborador as Colaborador;
       button.setAttribute('data-target', '#deleteColaboradorModal');
     }
 
